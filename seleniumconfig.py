186ced3f7842ae6bfd8d02d65509ec3817e9a953
Safae <safaesahlaoui2001@gmail.com>
2023-03-07 21:28:33 +0100

    tp commit

---
+++ seleniumconfig.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import  time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+class infow():
+    #driver =webdriver.Chrome()
+
+    query=None
+    def __init__(self):
+        option = Options().add_experimental_option("detach", True)
+        self.driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def get_info(self,query):
+        self.query=query
+        self.driver.get(url='https://www.wikipedia.org')
+
+        search= self.driver.find_element(By.XPATH,'//*[@id="searchInput"]')
+        search.click()
+        search.send_keys(query)
+        enter=self.driver.find_element(By.XPATH,'//*[@id="search-form"]/fieldset/button/i')
+        enter.click()
+        time.sleep(10)# just make my navigator wait , probably it works without it test it first
+
+#info= infow()
+#info.get_info('Maroc')
+
+
